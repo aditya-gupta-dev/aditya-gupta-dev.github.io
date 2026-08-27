8a61f8f64f53e2acf0ab60eda190fe3d76c6293b
Aditya Gupta <adityagupta2087@gmail.com>
2026-08-27 15:54:33 +0530

    clean start

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { ThemeProvider } from "@/components/providers/theme-provider";
-import { Outlet } from "react-router-dom";
-import { usePageTracking } from "@/lib/useAnalytics";
-import { AuthProvider } from "@/components/providers/auth-provider";
-
 export default function App() {
-  usePageTracking();
-
   return (
-    <ThemeProvider defaultTheme="system">
-      <AuthProvider>
-        <Outlet />
-      </AuthProvider>
-    </ThemeProvider>
+    <div>
+      Hi, from aditya
+    </div>
   );
 }
